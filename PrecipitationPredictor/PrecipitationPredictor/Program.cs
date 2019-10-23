a50497e97350e97c33f13940f9e21ab887dc75ad
David Barnes <david.barnes115@gmail.com>
2019-10-23 14:36:18 -0400

    app logic complete/fix needed to read json file

--- PrecipitationPredictor/PrecipitationPredictor/Program.cs
+++ PrecipitationPredictor/PrecipitationPredictor/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace PrecipitationPredictor
 {
@@ -10,9 +13,62 @@ namespace PrecipitationPredictor
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Precipitation Predictor!\nResults are based on historical precipitation data from the 27612 ZIP code.");
-            Console.WriteLine("Enter any date in MM/DD format (ex: 12/25).");
+            Console.WriteLine("Enter any date in MM/DD format (ex: 12/25) to find out predicted precipitation total for that date.");
             date = Console.ReadLine();
-            Console.WriteLine(date);
+            string result = PredictorResults.PrecipAverage(date);
+            Console.WriteLine(result);
         }
+    }
+
+    public static class PredictorResults
+    {
+        public static string PrecipAverage(string date)
+        {
+            using(StreamReader precipResults = new StreamReader(@"precip-records.json"))
+            {
+                        string contents = precipResults.ReadToEnd();
+
+                var res = JsonConvert.DeserializeObject<dynamic>(contents);
+
+                if (date == "")
+                {
+                    date = DateTime.Now.ToString("M/d");
+                }
+
+                double sum = 0;
+
+                int count = 0;
+
+                double avg = 0;
+
+                int x = 0;
+
+                for (int i = 0; i < 4492; i++)
+                {
+                    if (res[i].DATE == date)
+                    {
+                        string strPRCP = res[i].PRCP;
+                        double dblPRCP = Convert.ToDouble(strPRCP);
+
+                        sum += dblPRCP;
+
+                        count++;
+
+                        avg = sum / count;
+
+                    }
+
+                        if (res[i].DATE != date)
+                    {
+                        x++;
+                    }
+                }
+
+                Console.WriteLine("The predicted precipitation amount on " + date + " is " + avg + ".");
+            }
+
+            return "0";
+        }
+                
     }
 }
