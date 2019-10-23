57830442e64b391f8b7f91c8148b9200321bebfd
David Barnes <david.barnes115@gmail.com>
2019-10-23 13:45:16 -0400

    conv xls to json/set variables for avg calc

---
+++ PrecipitationPredictor/PrecipitationPredictor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrecipitationPredictor
+{
+    class Program
+    {
+        public static string date = "";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Precipitation Predictor!\nResults are based on historical precipitation data from the 27612 ZIP code.");
+            Console.WriteLine("Enter any date in MM/DD format (ex: 12/25).");
+            date = Console.ReadLine();
+            Console.WriteLine(date);
+        }
+    }
+}
