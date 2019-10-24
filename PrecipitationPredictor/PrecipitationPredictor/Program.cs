3a75e139830de763d5695794bcc4933d8b49eb0a
David Barnes <david.barnes115@gmail.com>
2019-10-24 13:47:38 -0400

    added comments and cleaned up code

--- PrecipitationPredictor/PrecipitationPredictor/Program.cs
+++ PrecipitationPredictor/PrecipitationPredictor/Program.cs
@@ -6,19 +6,32 @@ namespace PrecipitationPredictor
 {
     class Program
     {
+        // Declare date variable
         public static string date = "";
 
 
         static void Main(string[] args)
         {
+            // Write instructions for user to console
             Console.WriteLine("Welcome to Precipitation Predictor!");
             Console.WriteLine("\nResults are based on historical precipitation data from the 27612 ZIP code.");
-            Console.WriteLine("\nEnter any date in M/dd format (ex: 12/25 or 7/4) to find out predicted\nprecipitation total for that date.");
+            Console.WriteLine("\nEnter any date in Month/day format (ex: 12/7) to find out predicted\nprecipitation total for that date.");
             Console.WriteLine("\n(press 'enter' to use today's date)\n");
+
+            // Store user input in date variable
             date = Console.ReadLine();
-            Console.WriteLine("");
-            string result = PredictorResults.PrecipAverage(date);
-            Console.WriteLine(result);
+
+            // If user pressed enter for current date, write current date in M/dd format to console
+            if (date == "")
+            {
+                date = DateTime.Now.ToString("M/dd");
+                Console.WriteLine(date + "\n");
+            }
+
+            // Store PredictorResults in variable "prediction"
+            string prediction = PredictorResults.PrecipAverage(date);
+
+            Console.WriteLine(prediction);
 
         }
     }
@@ -27,60 +40,68 @@ namespace PrecipitationPredictor
     {
         public static string PrecipAverage(string date)
         {
+            // Read-in JSON file and store result in variable "contents"
             using(StreamReader precipResults = new StreamReader(@"/Users/davidbarnes/PrecipitationPredictor/PrecipitationPredictor/parsed-precip-data.json"))
             {
                         string contents = precipResults.ReadToEnd();
 
-                var res = JsonConvert.DeserializeObject<dynamic>(contents);
+                // Deserialize "contents" and store results in variable "results"
+                var results = JsonConvert.DeserializeObject<dynamic>(contents);
 
+                // If user pressed enter for current date, convert DateTime to M/dd string format
                 if (date == "")
                 {
                     date = DateTime.Now.ToString("M/dd");
                 }
 
-                //Console.WriteLine(res[0]);
-                //Console.WriteLine(res[0].DATE);
+                // If user input leading zero for single-digit month, remove it
+                string cleanDate = date.TrimStart('0');
 
-                string cleanedDate = date.TrimStart('0');
+                // Declare variable "runningSum" to store accumlated precipitation amounts
+                double runningSum = 0;
 
-                //Console.WriteLine(cleanedDate);
-
-                double sum = 0;
-
+                // Declare variable "count" to store number of objects with matching dates
                 int count = 0;
 
-                double avg = 0;
+                // Declare variable "averagePrecipitation" to store quotient from equation 
+                double averagePrecipitation = 0;
 
-                int x = 0;
-
+                // Iterate through elements of variable "results"
                 for (int i = 0; i < 4492; i++)
                 {
-                    if (res[i].parsedDate == cleanedDate)
+                    // Check if parsedDate for each object in results matches cleaned date from user input
+                    if (results[i].parsedDate == cleanDate)
                     {
-                        //Console.WriteLine(res[i]);
-                        string strPRCP = res[i].PRCP;
+                        // Write contents of matching objects to console to check accuracy
+                        // Console.WriteLine(results[i]);
+
+                        // Store string version of precipitation amount in variable "strPRCP"
+                        string strPRCP = results[i].PRCP;
+
+                        // Convert string precipitation amount to a double data type to allow for calculation
                         double dblPRCP = Convert.ToDouble(strPRCP);
 
+                        // Add precipitation amount to the running sum
+                        runningSum += dblPRCP;
 
-                        sum += dblPRCP;
-
+                        // Increase count by one for each object with matching date
                         count++;
 
-                        //Console.WriteLine(sum);
+                        // Check that runningSum is increasing accurately
+                        // Console.WriteLine(runningSum);
 
-                        avg = sum / count;
+                        // Obtain average precipitation amount
+                        averagePrecipitation = runningSum / count;
 
                     }
 
-                        if (res[i].parsedDate != cleanedDate)
-                    {
-                        x++;
-                    }
                 }
 
-                Console.WriteLine("The predicted precipitation amount on " + date + " is " + avg + ".");
+                // Write average precipitation amount for given date to the console
+                Console.WriteLine("The predicted precipitation amount on " + date + " is " + averagePrecipitation + ".");
             }
 
+            // Provide return for function
             return "\nThanks for using the Precipitation Predictor.";
         }
                 
